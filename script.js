d3562738cd4023182a1916b12c238c1fc68d1da6
Abd1h <abdlh.lllh@gmail.com>
2022-08-27 15:40:23 +0300

    featured-in animation

---
+++ script.js
@@ -0,0 +1,8 @@
+// const FeaturedInSection = document.querySelector(".featured-in-section");
+
+// document.addEventListener("scroll", function (e) {
+//   e.preventDefault();
+
+//   FeaturedInSection.style.animationName = "logos-right";
+//   console.log("fire");
+// });
