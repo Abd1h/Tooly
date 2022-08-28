4557c8562dea3e3932baf2ff7993fe59bb27fad9
Abd1h <abdlh.lllh@gmail.com>
2022-08-28 19:49:06 +0300

    finished html of reviews section

--- script.js
+++ script.js
@@ -6,3 +6,36 @@
 //   FeaturedInSection.style.animationName = "logos-right";
 //   console.log("fire");
 // });
+var checkScrollSpeed = (function (settings) {
+  settings = settings || {};
+
+  var lastPos,
+    newPos,
+    timer,
+    delta,
+    delay = settings.delay || 50; // in "ms" (higher means lower fidelity )
+
+  function clear() {
+    lastPos = null;
+    delta = 0;
+  }
+
+  clear();
+
+  return function () {
+    newPos = window.scrollY;
+    if (lastPos != null) {
+      // && newPos < maxScroll
+      delta = newPos - lastPos;
+    }
+    lastPos = newPos;
+    clearTimeout(timer);
+    timer = setTimeout(clear, delay);
+    return delta;
+  };
+})();
+
+// listen to "scroll" event
+window.onscroll = function () {
+  console.log(checkScrollSpeed());
+};
