dc7503e48c59feb0e7ff163be8fdb6ec0ce2ff07
Abd1h <abdlh.lllh@gmail.com>
2022-09-15 13:15:31 +0300

    burgerNav-done

---
+++ JavaScript/mobileNavView.js
@@ -0,0 +1,27 @@
+const burgerBtn = document.querySelector(".burger-btn");
+const burgerMenu = document.querySelector(".nav--links");
+const btnLines = document.querySelectorAll(".line");
+const navLinks = document.querySelectorAll(".link");
+
+// "open" class that opens burgerMenu
+// "line-1 ,line-2,line-3" classes that manipulate burgerBtn shape
+
+const toggleBurgerMenu = function () {
+  // 1) slide the menu to the view
+  burgerMenu.classList.toggle("open");
+  // 2) change burger btn style (open/close)
+  btnLines.forEach((line, i) => {
+    line.classList.toggle(`line-${i + 1}`);
+  });
+};
+
+// when user click burger btn
+burgerBtn.addEventListener("click", function () {
+  toggleBurgerMenu();
+});
+// close menu when user click on a link
+navLinks.forEach((link) =>
+  link.addEventListener("click", function () {
+    toggleBurgerMenu();
+  })
+);
