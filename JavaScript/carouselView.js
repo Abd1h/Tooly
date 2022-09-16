991ec1b057c30abb9631679cfc3490ff065f2a21
Abd1h <abdlh.lllh@gmail.com>
2022-09-16 11:30:29 +0300

    carouselView-done

--- JavaScript/carouselView.js
+++ JavaScript/carouselView.js
@@ -12,37 +12,55 @@ const reviewsNumber = reviews.length;
 // *btnRight clicked*
 // currentReview =2 | -200% -100% 0%
 
-const moveToRight = function () {
+const moveToReview = function () {
   reviews.forEach((review, i) => {
     review.style.transform = ` translateX(${(i - currentReview) * 100}%) `;
   });
 };
-moveToRight();
+moveToReview(); //to set the initial position for all reviews
 
-const moveToLeft = function () {
-  reviews.forEach((review, i) => {
-    review.style.transform = ` translateX(${(i - currentReview) * 100}%) `;
+const UpdateActiveDot = function () {
+  caouselDots.forEach((dot) => {
+    // 1) remove dot-active class from all dots
+    dot.classList.remove('dot-active');
+    // 2) adding dot-active to the matching dot
+    const dotNumber = +dot.dataset.dot; //1 , 2 , 3
+
+    if (dotNumber === currentReview + 1) {
+      dot.classList.add('dot-active');
+    }
   });
 };
-moveToLeft();
+UpdateActiveDot(); //to set the initial active dot
 
 btnRight.addEventListener('click', function () {
   // 1)if its the last review than go back to the first
   if (reviewsNumber - 1 === currentReview) {
     currentReview = 0;
-    moveToRight();
   }
   // moveing and updating the active dot
   else {
     currentReview++;
-    moveToRight();
   }
+  moveToReview();
+  UpdateActiveDot();
 });
 
 btnLeft.addEventListener('click', function () {
-  // 1)if its the last review than go back to the first
+  // 1)if its the first review than do nothing
   if (currentReview === 0) return;
   // moveing and updating the active dot
   currentReview--;
-  moveToLeft();
+  moveToReview();
+  UpdateActiveDot();
+});
+
+caouselDots.forEach((dot) => {
+  dot.addEventListener('click', function () {
+    currentReview = dot.dataset.dot - 1;
+
+    // moveing and updating the active dot
+    moveToReview();
+    UpdateActiveDot();
+  });
 });
