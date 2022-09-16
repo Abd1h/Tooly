dbb6a5eb3e45ea0c430c795a772f8701456d78ec
Abd1h <abdlh.lllh@gmail.com>
2022-09-16 14:41:42 +0300

    mobileNav-overflow

--- JavaScript/mobileNavView.js
+++ JavaScript/mobileNavView.js
@@ -1,14 +1,14 @@
-const burgerBtn = document.querySelector(".burger-btn");
-const burgerMenu = document.querySelector(".nav--links");
-const btnLines = document.querySelectorAll(".line");
-const navLinks = document.querySelectorAll(".link");
+const burgerBtn = document.querySelector('.burger-btn');
+const burgerMenu = document.querySelector('.nav-links-container');
+const btnLines = document.querySelectorAll('.line');
+const navLinks = document.querySelectorAll('.link');
 
 // "open" class that opens burgerMenu
 // "line-1 ,line-2,line-3" classes that manipulate burgerBtn shape
 
 const toggleBurgerMenu = function () {
   // 1) slide the menu to the view
-  burgerMenu.classList.toggle("open");
+  burgerMenu.classList.toggle('open');
   // 2) change burger btn style (open/close)
   btnLines.forEach((line, i) => {
     line.classList.toggle(`line-${i + 1}`);
@@ -16,12 +16,12 @@ const toggleBurgerMenu = function () {
 };
 
 // when user click burger btn
-burgerBtn.addEventListener("click", function () {
+burgerBtn.addEventListener('click', function () {
   toggleBurgerMenu();
 });
 // close menu when user click on a link
 navLinks.forEach((link) =>
-  link.addEventListener("click", function () {
+  link.addEventListener('click', function () {
     toggleBurgerMenu();
   })
 );
